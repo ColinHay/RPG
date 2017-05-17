a3a33a64df3a85693fb96ec3cab38ceaf1befa03
Colin Hay <chay@ithaca.edu>
2017-05-16 21:28:30 -0400

    Added Slack integration on colhay.slack.com
    The program now outputs all posts to slack and only pushes new content

--- RPG.py
+++ RPG.py
@@ -1,32 +1,47 @@
 #RPG - Reddit Post Getter#
-#           1.0          #          
+#           2.0          #          
 ##########################
 
-#import praw
+#import modules
 import praw
+import time
+from slacker import Slacker
 
 #create reddit instance
 reddit = praw.Reddit(client_id='vaBsxDjQvk5jWA',
                      client_secret='81M6u2rXtrP-kGF1KKg99LSwRuY',
-                     user_agent='Windows:RPG:1.0 (by /u/coli_ha)')
+                     user_agent='Windows:RPG:2.0 (by /u/coli_ha)')
 
-#Lists of subreddits and keywords to use
+#connect to slack
+slack = Slacker('xoxb-183678733280-v4ZLYDSWPOGVDzwbvfLLfZB2')
+
+#Lists of subreddits, keywords, and posted items as well as a dictionary for newPosts
 subreddits = ['space', 'astronomy', 'astrophotography',
               'cosmos', 'cosmology', 'telescopes',
               'moon', 'comets']
 
 keywords = ['space', 'telescope', 'slooh',
-            'moon', 'eclipse', 'astronomy']
+            'moon', 'eclipse', 'astronomy',
+            'planet']
 
-#For loops for printing relevant data
-for subredditName in subreddits:
-    print('')
-    print('==========')
-    print('/r/'+subredditName)
-    print('==========')
-    for post in reddit.subreddit(subredditName).new(limit=10):
-        for keyword in keywords:
-            if keyword in post.title:
-                print(post.title)
-                print('https://www.reddit.com/r/'+subredditName+'/comments/'+post.id)
-                print('')
+newPosts = {}
+posted = []
+
+while True:
+    #For loops populate newPosts dictionary with relevant data
+    for subredditName in subreddits:
+        for post in reddit.subreddit(subredditName).new(limit=10):
+            for keyword in keywords:
+                if keyword in post.title:
+                    if post.id not in newPosts :
+                        newPosts.update({post.id:'\n\n\n----------\n/r/'+subredditName+'\n'+post.title+'\nhttps://www.reddit.com/r/'+subredditName+'/comments/'+post.id+'\n----------'})
+
+    #For loop prints gathered data to console and adds it to posted list
+    for id in newPosts:
+        if id not in posted:
+            posted.append(id)
+            print(newPosts[id])
+            slack.chat.post_message('#test', newPosts[id])
+
+    #Refresh timer
+    time.sleep(600)
